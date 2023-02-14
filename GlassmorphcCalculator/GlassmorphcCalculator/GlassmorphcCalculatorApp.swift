7ebc8446f970087d1e17938fc22d548d0550f692
Andrea <andreamasturzo@outlook.comandreamasturzo@outlook.comandreamasturzo@outlook.com>
2023-02-14 09:16:47 +0100

    First Commit

---
+++ GlassmorphcCalculator/GlassmorphcCalculator/GlassmorphcCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  GlassmorphcCalculatorApp.swift
+//  GlassmorphcCalculator
+//
+//  Created by Andrea Masturzo on 14/02/23.
+//
+
+import SwiftUI
+
+@main
+struct GlassmorphcCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
